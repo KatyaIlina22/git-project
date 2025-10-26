4a7626c69c745052c7cda2148c2d282824acc74f
KatyaIlina22 <katya.katerina2222@gmail.com>
2025-10-26 18:31:39 +0200

    Add files via upload

---
+++ практика макет git/js/script.js
@@ -0,0 +1,8 @@
+"use strict"
+let ducumentActions = (e) =>{
+    const targetElement=e.target
+    if(targetElement.closest('.icon-menu')){
+        document.documentElement.classList.toggle("open-menu")
+    }
+}
+document.addEventListener('click',ducumentActions)
